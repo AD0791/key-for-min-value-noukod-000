3216d3323bd5cd6ba8713cf6f6daf5dc2a4f05fd
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-25 01:22:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
+  lkey = nil
+  lv = nil
   hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
+    if lv == nil || v < lv
+      lv = v
+      lkey = k
     end
   end
-  lowest_key
+  lkey
 end
